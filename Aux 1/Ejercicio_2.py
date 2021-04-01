def94706ef2d6c2685d473971dee21e15e0a875d
Tomas <tomas.alvaradopi@gmail.com>
2021-04-01 19:53:20 -0300

    Update Ejercicio_2.py
    Implementacion de arboles

--- Aux 1/Ejercicio_2.py
+++ Aux 1/Ejercicio_2.py
@@ -263,35 +263,19 @@ def create_sky(y0, y1):
 
     return Shape(vertices, indices)
 
-def create_ocean(y0, y1):
-
-    # Defining the location and colors of each vertex  of the shape
-    vertices = [
-    #   positions        colors
-        -1.0, y0, 0.0,  0.0, 0.0, 0.5,
-         1.0, y0, 0.0,  0.0, 0.0, 0.5,
-         1.0, y1, 0.0,  0.2, 0.4, 1.0,
-        -1.0, y1, 0.0,  0.2, 0.4, 1.0]
-
-    # Defining connections among vertices
-    # We have a triangle every 3 indices specified
-    indices = [0, 1, 2,
-                2, 3, 0]
-
-    return Shape(vertices, indices)
 
 def create_island(x0, y0, width, height):
 
     # Defining the location and colors of each vertex  of the shape
     vertices = [
     #   positions                           colors
-         0.6, 0.0 + y0, 0.0, 0.63, 0.25, 0.0,
-         0.6, -1.0, 0.0, 0.63, 0.25, 0.0,
-         -0.6, 0.0 + y0, 0.0, 0.63, 0.25, 0.0,
+         x0, 0.0 + y0, 0.0, 0.0, 0.3, 0.0,
+         x0, -1.0, 0.0, 0.0, 0.4, 0.0,
+         -x0, 0.0 + y0, 0.0, 0.0, 0.3, 0.0,
 
-         -0.6, 0.0 + y0, 0.0, 0.63, 0.25, 0.0,
-         -0.6, -1.0, 0.0, 0.63, 0.25, 0.0,
-         0.6, -1.0, 0.0, 0.63, 0.25, 0.0]
+         -x0, 0.0 + y0, 0.0, 0.0, 0.3, 0.0,
+         -x0, -1.0, 0.0, 0.0, 0.4, 0.0,
+         x0, -1.0, 0.0, 0.0, 0.4, 0.0]
 
     indices = [0, 1, 2,
                 3, 4, 5]
@@ -309,12 +293,131 @@ def create_volcano(x0, y0, width, height):
 
          x0 + width*0.2, y0, 0.0,  0.3, 0.15, 0.1,
          x0 + width, y0, 0.0,  0.3, 0.15, 0.1,
-         x0 + width*0.6, y0 + height, 0.0, 0.6, 0.31, 0.17]
+         x0 + width*0.6, y0 + height, 0.0, 0.6, 0.31, 0.17,
+         #lava
+         x0 + 0.337, y0 + height - 0.03, 0.0, 0.3, 0.0, 0.0,
+         x0 + width*0.5, y0 + height - 0.13, 0.0, 0.8, 0.0, 0.0,
+         x0 + width*0.6 - 0.016, y0 + height - 0.03, 0.0, 0.3, 0.0, 0.0]
 
     # Defining connections among vertices
     # We have a triangle every 3 indices specified
     indices = [0, 1, 2,
-                3, 4, 5]
+                3, 4, 5,
+                6, 7, 8]
+
+    return Shape(vertices, indices)
+def create_arbol(x0, y0, width, height):
+
+    # Defining the location and colors of each vertex  of the shape
+    vertices = [
+    #   positions        colors
+         #Arbol 1
+         #Tronco
+         -0.2, -0.5, 0.0, 0.5, 0.3, 0.0,
+         -0.2, -0.6, 0.0, 0.5, 0.3, 0.0,
+         -0.25, -0.6, 0.0, 0.5, 0.3, 0.0,
+         -0.25, -0.5, 0.0, 0.5, 0.3, 0.0,
+         #Hojas
+         -0.27, -0.5, 0.0, 0.0, 0.8, 0.0,
+         -0.27, -0.4, 0.0, 0.0, 0.8, 0.0,
+         -0.18, -0.5, 0.0, 0.0, 0.8, 0.0,
+         -0.18, -0.4, 0.0, 0.0, 0.8, 0.0,
+         #Arbol 2
+         #Tronco
+         -0.2 + 0.5, -0.5, 0.0, 0.5, 0.3, 0.0,
+         -0.2 + 0.5, -0.6, 0.0, 0.5, 0.3, 0.0,
+         -0.25 + 0.5, -0.6, 0.0, 0.5, 0.3, 0.0,
+         -0.25 + 0.5, -0.5, 0.0, 0.5, 0.3, 0.0,
+         #Hojas
+         -0.27 + 0.5, -0.5, 0.0, 0.0, 0.8, 0.0,
+         -0.27 + 0.5, -0.4, 0.0, 0.0, 0.8, 0.0,
+         -0.18 + 0.5, -0.5, 0.0, 0.0, 0.8, 0.0,
+         -0.18 + 0.5, -0.4, 0.0, 0.0, 0.8, 0.0,
+        
+         #Arbol 3
+         #Tronco
+         -0.2 - 0.2, -0.5 + 0.1, 0.0, 0.5, 0.3, 0.0,
+         -0.2 - 0.2, -0.6 + 0.1, 0.0, 0.5, 0.3, 0.0,
+         -0.25 - 0.2, -0.6 + 0.1, 0.0, 0.5, 0.3, 0.0,
+         -0.25 - 0.2, -0.5 + 0.1, 0.0, 0.5, 0.3, 0.0,
+         #Hojas
+         -0.27 - 0.2, -0.5 + 0.1, 0.0, 0.0, 0.8, 0.0,
+         -0.27 - 0.2, -0.4 + 0.1, 0.0, 0.0, 0.8, 0.0,
+         -0.18 - 0.2, -0.5 + 0.1, 0.0, 0.0, 0.8, 0.0,
+         -0.18 - 0.2, -0.4 + 0.1, 0.0, 0.0, 0.8, 0.0,
+         
+         #Arbol 3
+         #Tronco
+         -0.2*0.8 +0.6, -0.5*0.8 + 0.3, 0.0, 0.5, 0.3, 0.0,
+         -0.2*0.8 +0.6, -0.6*0.8 + 0.3, 0.0, 0.5, 0.3, 0.0,
+         -0.25*0.8 +0.6, -0.6*0.8 + 0.3, 0.0, 0.5, 0.3, 0.0,
+         -0.25*0.8 +0.6, -0.5*0.8 + 0.3, 0.0, 0.5, 0.3, 0.0,
+         #Hojas
+         -0.27*0.8 +0.6, -0.5*0.8 + 0.3, 0.0, 0.0, 0.8, 0.0,
+         -0.27*0.8 +0.6, -0.4*0.8 + 0.3, 0.0, 0.0, 0.8, 0.0,
+         -0.18*0.8 +0.6, -0.5*0.8 + 0.3, 0.0, 0.0, 0.8, 0.0,
+         -0.18*0.8 +0.6, -0.4*0.8 + 0.3, 0.0, 0.0, 0.8, 0.0,
+         
+         #Arbol 4
+         #Tronco
+         -0.2*0.7 -0.1, -0.5*0.7 + 0.2, 0.0, 0.5, 0.3, 0.0,
+         -0.2*0.7 -0.1, -0.6*0.7 + 0.2, 0.0, 0.5, 0.3, 0.0,
+         -0.25*0.7 -0.1, -0.6*0.7 + 0.2, 0.0, 0.5, 0.3, 0.0,
+         -0.25*0.7 -0.1, -0.5*0.7 + 0.2, 0.0, 0.5, 0.3, 0.0,
+         #Hojas
+         -0.27*0.7 -0.1, -0.5*0.7 + 0.2, 0.0, 0.0, 0.8, 0.0,
+         -0.27*0.7 -0.1, -0.4*0.7 + 0.2, 0.0, 0.0, 0.8, 0.0,
+         -0.18*0.7 -0.1, -0.5*0.7 + 0.2, 0.0, 0.0, 0.8, 0.0,
+         -0.18*0.7 -0.1, -0.4*0.7 + 0.2, 0.0, 0.0, 0.8, 0.0]
+
+    # Defining connections among vertices
+    # We have a triangle every 3 indices specified
+    indices = [0, 1, 2,#Arbol 1
+                2, 3, 0,
+
+                4, 5, 6,
+                5, 6, 7,
+                
+                #Arbol 2
+                8, 9, 10,
+                10, 11, 8,
+
+                12, 13, 14,
+                13, 14, 15,
+                
+                #Arbol 3
+                16, 17, 18,
+                18, 19, 16,
+                
+                20, 21, 22,
+                21, 22, 23,
+                
+                #Arbol 4
+                24, 25, 26,
+                26, 27, 24,
+                
+                28, 29, 30,
+                29, 30, 31,
+                
+                #Arbol 5
+                32, 33, 34,
+                34, 35, 32,
+                
+                36, 37, 38,
+                37, 38, 39]
+
+    return Shape(vertices, indices)
+
+def create_river(x0, y0, width, height):
+
+    # Defining the location and colors of each vertex  of the shape
+    vertices = [
+    #   positions        colors
+         ]
+
+    # Defining connections among vertices
+    # We have a triangle every 3 indices specified
+    indices = []
 
     return Shape(vertices, indices)
 
@@ -344,33 +447,34 @@ if __name__ == "__main__":
     sunsetPipeline = SunsetShaderProgram()
 
     # Creating shapes on GPU memory
-    sky_shape = create_sky(y0=-0.2, y1=1.0)
+    sky_shape = create_sky(y0=0.2, y1=1.0)
     gpu_sky = GPUShape().initBuffers()
     simplePipeline.setupVAO(gpu_sky)
     greenPipeline.setupVAO(gpu_sky)
     sunsetPipeline.setupVAO(gpu_sky)
     gpu_sky.fillBuffers(sky_shape.vertices, sky_shape.indices, GL_STATIC_DRAW)
 
-    ocean_shape = create_ocean(y0=-1.0, y1=-0.2)
-    gpu_ocean = GPUShape().initBuffers()
-    simplePipeline.setupVAO(gpu_ocean)
-    greenPipeline.setupVAO(gpu_ocean)
-    sunsetPipeline.setupVAO(gpu_ocean)
-    gpu_ocean.fillBuffers(ocean_shape.vertices, ocean_shape.indices, GL_STATIC_DRAW)
 
-    island_shape = create_island(x0=-0.8, y0=-0.2, width=1.6, height=0.8)
+    island_shape = create_island(x0=-1.0, y0=0.2, width=1.6, height=0.8)
     gpu_island = GPUShape().initBuffers()
     simplePipeline.setupVAO(gpu_island)
     greenPipeline.setupVAO(gpu_island)
     sunsetPipeline.setupVAO(gpu_island)
     gpu_island.fillBuffers(island_shape.vertices, island_shape.indices, GL_STATIC_DRAW)
 
-    volcano_shape = create_volcano(x0=-0.3, y0=-0.22, width=0.6, height=0.4)
+    volcano_shape = create_volcano(x0=-0.35, y0=0.1, width=0.8, height=0.5)
     gpu_volcano = GPUShape().initBuffers()
     simplePipeline.setupVAO(gpu_volcano)
     greenPipeline.setupVAO(gpu_volcano)
     sunsetPipeline.setupVAO(gpu_volcano)
     gpu_volcano.fillBuffers(volcano_shape.vertices, volcano_shape.indices, GL_STATIC_DRAW)
+
+    arbol_shape = create_arbol(x0=-0.35, y0=0.1, width=0.8, height=0.5)
+    gpu_arbol = GPUShape().initBuffers()
+    simplePipeline.setupVAO(gpu_arbol)
+    greenPipeline.setupVAO(gpu_arbol)
+    sunsetPipeline.setupVAO(gpu_arbol)
+    gpu_arbol.fillBuffers(arbol_shape.vertices, arbol_shape.indices, GL_STATIC_DRAW)
 
 
     # Setting up the clear screen color
@@ -392,29 +496,29 @@ if __name__ == "__main__":
         if (controller.effect1):
             glUseProgram(greenPipeline.shaderProgram)
             greenPipeline.drawCall(gpu_sky)
-            greenPipeline.drawCall(gpu_ocean)
             greenPipeline.drawCall(gpu_island)
-            greenPipeline.drawCall(gpu_volcano)
+            greensPipeline.drawCall(gpu_volcano)
+            greensPipeline.drawCall(gpu_arbol)
         elif (controller.effect2):
             glUseProgram(sunsetPipeline.shaderProgram)
             sunsetPipeline.drawCall(gpu_sky)
-            sunsetPipeline.drawCall(gpu_ocean)
             sunsetPipeline.drawCall(gpu_island)
             sunsetPipeline.drawCall(gpu_volcano)
+            sunsetPipeline.drawCall(gpu_arbol)
         else:
             glUseProgram(simplePipeline.shaderProgram)
             simplePipeline.drawCall(gpu_sky)
-            simplePipeline.drawCall(gpu_ocean)
             simplePipeline.drawCall(gpu_island)
             simplePipeline.drawCall(gpu_volcano)
+            simplePipeline.drawCall(gpu_arbol)
 
         # Once the render is done, buffers are swapped, showing only the complete scene.
         glfw.swap_buffers(window)
 
     # freeing GPU memory
     gpu_sky.clear()
-    gpu_ocean.clear()
     gpu_island.clear()
     gpu_volcano.clear()
+    gpu_arbol.clear()
 
     glfw.terminate()
