dd1eba58276a8e189c9716ce5d987906a8a3aa65
Tomas <tomas.alvaradopi@gmail.com>
2021-06-21 00:00:58 -0400

    final 1ra entrega
    se termino el tobogan pero no se ve en pantalla

--- Tarea2/water_slide.py
+++ Tarea2/water_slide.py
@@ -15,20 +15,21 @@ import grafica.ex_curves as cv
 import grafica.lighting_shaders as ls
 import openmesh as om 
 import ej6
+import random
 ############################################################################
 
 def CurvaTobogan(N):
     
-    P0 = np.array([[-1.3, -0.4, 1]]).T
-    P1 = np.array([[-1.1, -0.299, 0.9]]).T
-    P2 = np.array([[-0.341, -0.129, 0.83]]).T
-    P3 = np.array([[0.121, -0.313, 0.6]]).T
-    P4 = np.array([[0.55, -0.36, 0.7]]).T
-    P5 = np.array([[1.12, -0.39, 0.77]]).T
-    P6 = np.array([[1.44, -0.51, 0.5]]).T
-    P7 = np.array([[1.93, -0.31, 0.44]]).T
-    P8 = np.array([[2.33, -0.43, 0.3]]).T
-    P9 = np.array([[2.46, -0.22, 0.2]]).T
+    P0 = np.array([[-1.3, -0.4, 3]]).T
+    P1 = np.array([[-1.1, -0.299, 2.7]]).T
+    P2 = np.array([[-0.341, -0.129, 2.83]]).T
+    P3 = np.array([[0.121, -0.313, 2.6]]).T
+    P4 = np.array([[0.55, -0.36, 2.7]]).T
+    P5 = np.array([[1.12, -0.39, 2.77]]).T
+    P6 = np.array([[1.44, -0.51, 2.5]]).T
+    P7 = np.array([[1.93, -0.31, 2.44]]).T
+    P8 = np.array([[2.33, -0.43, 2.3]]).T
+    P9 = np.array([[2.46, -0.22, 2.2]]).T
     
     CM1 = cv.CatmullMatrix(P0, P1, P2, P3)
     CM2 = cv.CatmullMatrix(P1, P2, P3, P4)
@@ -60,30 +61,37 @@ def tobogan(N,n):
     curva = CurvaTobogan(N)
     mesh = om.TriMesh()
     r = 0.3
+    n = 77
+    f = 2 * np.pi / 7
     
     vertices= []
-    for i in range(n+1):
-        x = 0
-        z = np.cos(i*np.pi) * r
-        y = np.sin(i*np.pi) * r
-        vertices.append([x,y,z])
 
-    for i in range(len(curva)-1):
-        vertices[i][0] += curva[i][0]
-        vertices[i][1] += curva[i][1]
-        vertices[i][2] += curva[i][2]
+    for i in range(n):
+        for j in range(7):
+            x = 0
+            z = np.cos(j*f) * r
+            y = np.sin(j*f) * r
+            vertices.append([x,y,z])
+
+    for i in range(len(vertices)):
+        #vertices[i][0] += curva[i][0]
+        #vertices[i][1] += curva[i][1]
+        #vertices[i][2] += curva[i][2]
         mesh.add_vertex([vertices[i][0],vertices[i][1],vertices[i][2]])
     
-    def ind(i,j,n):
-        x = i*(n+j)
-        return x
 
+    def ind(i,j):
+        x = (i*7) + j
+        return x
+    print(len(mesh.points()))
     for i in range(len(curva)-1):
-        for j in range(n):
-            indice1 = ind(i,j,n)
-            indice2 = ind(i,j+1,n)
-            indice3 = ind(i+1,j,n)
-            indice4 = ind(i+1,j+1,n)
+        print(i)
+        for j in range(6):
+
+            indice1 = ind(i,j)
+            indice2 = ind(i,j+1)
+            indice3 = ind(i+1,j)
+            indice4 = ind(i+1,j+1)
 
             vertexs = list(mesh.vertices())
 
@@ -98,7 +106,7 @@ def get_vertex_and_indexes(mesh):
     vertices = []
 
     for i in mesh.points():
-        
+        print(i)
         vertices += i.tolist()
         vertices += [random.uniform(0,1),random.uniform(0,1),random.uniform(0,1)]
     
@@ -106,7 +114,7 @@ def get_vertex_and_indexes(mesh):
     indices = []
     
     for i in faces:
-
+        
         indexes_faces = mesh.fv(i)
 
         for j in indexes_faces:
@@ -249,33 +257,34 @@ if __name__ == "__main__":
 
 ###########################################################################################
     # Creating shapes on GPU memory
+    pipeline = es.SimpleModelViewProjectionShaderProgram()
     phongPipeline = ls.SimplePhongShaderProgram()
     #Tobogan = createToboganNode(0.5,0.5,0.5,phongPipeline)
     
     skybox = create_skybox(textureShaderProgram)
     floor = create_floor(textureShaderProgram)
     #escena_tobogan = dibujo_de_tobogan(colorShaderProgram)
-    barco_mov = ej6.create_barco(colorShaderProgram)
+    #barco_mov = ej6.create_barco(colorShaderProgram)
 
-    coor_curva = CurvaTobogan(686)
+    #coor_curva = CurvaTobogan(686)
     
-    mov_boat = sg.findNode(barco_mov, "barco")
+    #mov_boat = sg.findNode(barco_mov, "barco")
     
-    movimiento_barco = ej6.Movimientodelbarco(0.1)
-    movimiento_barco.set_curva(coor_curva)
-    movimiento_barco.set_model(mov_boat)
-    movimiento_barco.set_controller(controller)
+    #movimiento_barco = ej6.Movimientodelbarco(0.1)
+    #movimiento_barco.set_curva(coor_curva)
+    #movimiento_barco.set_model(mov_boat)
+    #movimiento_barco.set_controller(controller)
 
-    mesh = tobogan(81, 4)
+    mesh = tobogan(81, 74)
     mesh_vertex , mesh_indx = get_vertex_and_indexes(mesh)
     gpumesh = es.GPUShape().initBuffers()
-    pipeline.setupVao(gpumesh)
+    pipeline.setupVAO(gpumesh)
     gpumesh.fillBuffers(mesh_vertex,mesh_indx,GL_STATIC_DRAW)
 ###########################################################################################
-
+    glUseProgram(pipeline.shaderProgram)
     # View and projection
     projection = tr.perspective(60, float(width)/float(height), 0.1, 100)
-
+    print('ok')
     while not glfw.window_should_close(window):
         # Using GLFW to check for input events
         glfw.poll_events()
@@ -289,17 +298,24 @@ if __name__ == "__main__":
         # Clearing the screen in both, color and depth
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        movimiento_barco.update()
+        
 
 ###########################################################################
-
-        at_x = controller.eye[0] + np.cos(controller.theta)
-        at_y = controller.eye[1] + np.sin(controller.theta)
-        controller.at = np.array([at_x, at_y, controller.at[2]])
-        view = tr.lookAt(controller.eye, controller.at, controller.up)
-
+        
+        #at_x = controller.eye[0] + np.cos(controller.theta)
+        #at_y = controller.eye[1] + np.sin(controller.theta)
+        #controller.at = np.array([at_x, at_y, controller.at[2]])
+        #view = tr.lookAt(1, controller.at, controller.up)
+        view = tr.lookAt(
+            [1,1,1],
+            np.array([1.12, -0.39, 2.77]),
+            np.array([0,0,1])
+        )
+        
 ###########################################################################
-        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"),1,GL_TRUE,tr.uniformscale(1))
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE, tr.uniformScale(1))
         pipeline.drawCall(gpumesh)
 
         # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
